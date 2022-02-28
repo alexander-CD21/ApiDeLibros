526279c8194fb81e808d6acfac78d61729bb2945
alexander-CD21 <alexander.guillen.v@uni.pe>
2022-02-28 18:52:48 -0500

    "agregando vistas y dejando las apis"

---
+++ mvc/src/main/java/com/codingdojo/mvc/controllers/BookController.java
@@ -0,0 +1,27 @@
+package com.codingdojo.mvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.services.BookServices;
+
+@Controller
+public class BookController {
+	@Autowired
+	private  BookServices bookServices;
+	//otra opcion al autowired
+	//public BookController(BookServices bookServices) {
+	//	this.bookServices = bookServices;
+	//}
+
+	@RequestMapping("/book/{id}")
+	public String  show(@PathVariable("id") Long id, Model model ) {
+		Book book =bookServices.findBook(id);
+		model.addAttribute("book",book);
+		return "show.jsp";
+	}
+}
